3df5cea79fdb48380d9f3c849b1c7a494158ba3e
Gustavo Aparicio Mendoza <90279094+GustavoAparicio@users.noreply.github.com>
2022-09-02 19:02:59 -0700

    Update registration.route.js

--- routes/registrations.route.js/registration.route.js
+++ routes/registrations.route.js/registration.route.js
@@ -14,6 +14,7 @@ const registrationsRouter = express.Router();
 
 registrationsRouter.get('/', getAllRegistrations);
 registrationsRouter.post('/', createRegistration);
+
 registrationsRouter.get('/:id', getRegistrationById);
 registrationsRouter.patch('/:id', updateRegistration);
 registrationsRouter.delete('/:id', cancelRegistration);
